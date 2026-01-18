0992eef66acebded15c41bc3e99c90ce05292256
crucialize <crucialize@foxmail.com>
2026-01-18 12:51:10 +0800

    1

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -22,8 +22,8 @@ fn main() {
 
     let mut results = Vec::new();
     for handle in handles {
-        // TODO: Collect the results of all threads into the `results` vector.
-        // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        let handle_result = handle.join().unwrap();
+        results.push(handle_result);
     }
 
     if results.len() != 10 {
